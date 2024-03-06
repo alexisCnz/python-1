85006377bf359d90b7587ec9eea9e2d6630541d1
alexisCnz <161972542+alexisCnz@users.noreply.github.com>
2024-03-06 10:20:55 -0300

    archivo inicial

---
+++ python1.py
@@ -0,0 +1,17 @@
+
+edad = 19
+
+if edad >= 18:
+    print ("eres mayor de edad")
+elif edad>4:
+    print ("eres un lolito")
+else:
+    print("eres un bebe")
+           
+#variables
+
+nombre = "lolo"
+nombre2=nombre.upper()
+nombre3=nombre.lower()
+print(nombre2)
+print(nombre3)   
